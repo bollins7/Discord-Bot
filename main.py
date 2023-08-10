d0540228ac0d6c264e26d46adeba2e61bf032592
Chris <37301037+bollins7@users.noreply.github.com>
2023-08-09 23:01:02 -0500

    Message reply
    What the hell am i doing

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import discord
 from discord.ext import commands
 
+
 from apis import *
 
 intents = discord.Intents.all()
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
     print ("I'm Ready!")
 
-bot.run(TOKEN)
+if message.content.startswith('!hello'):
+    await message.channel.send('Hello bro!')
+
+bot.run (TOKEN)
